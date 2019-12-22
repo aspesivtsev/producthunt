6459af14235db0cc69a156baf61b1e7c900106a5
AnatolySP <anspesiv@yahoo.com>
2019-12-22 13:12:45 +0300

    auth works

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import auth
 
 def signup(request):
-    return render(request, 'accounts/signup.html')
+    if request.method == 'POST':
+        #User wants to signup
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.get(username=request.POST['username'])
+                return render(request, 'accounts/signup.html', {'error':'User name has already been taken'})
+            except User.DoesNotExist:
+                user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+                auth.login(request, user)
+                return redirect('home')
+        else:
+            return render(request, 'accounts/signup.html', {'error': 'Passwords must match!'})
+    else:
+        return render(request, 'accounts/signup.html')
 
 def login(request):
     return render(request, 'accounts/login.html')
 
 def logout(request):
+    # TODO Need to route to a home page and not to forget to logout
     return render(request, 'accounts/signup.html')
 
 
