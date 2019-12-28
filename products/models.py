9cea05d90e20866f7ad8c93c7a229567f6078271
AnatolySP <anspesiv@yahoo.com>
2019-12-28 17:19:33 +0300

    model added but not migrated

--- products/models.py
+++ products/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    url = models.CharField(max_length=2082)
+    pub_date = models.DateTimeField()
+    mod_date = models.DateTimeField(auto_now=True)
+    votes_total = models.IntegerField()
+    image = models.ImageField(upload_to='images/products/', blank=True)
+    icon = models.ImageField(upload_to='images/blog/', blank=True)
+    body = models.TextField()
+
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
+
+    def __str__(self):
+        return self.title
+
+    def mod_date_pretty(self):
+        return self.mod_date.strftime('%b %e %Y, %H:%M')
+
+    def summary(self):
+        '''This is shortening of the body text, like a small preview... currently it takes first 20 words.'''
+        return " ".join(self.body.split()[:20])+"..."
